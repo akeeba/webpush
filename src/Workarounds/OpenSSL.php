adf8b7657c2e6a188f526d7bf314776ec4b767c8
Nicholas K. Dionysopoulos <nicholas@akeeba.com>
2022-10-18 12:50:45 +0300

    Missing DocBlock

--- src/Workarounds/OpenSSL.php
+++ src/Workarounds/OpenSSL.php
@@ -1,4 +1,10 @@
 <?php
+/**
+ * @package   akeebabackup
+ * @copyright Copyright (c)2006-2022 Nicholas K. Dionysopoulos / Akeeba Ltd
+ * @license   GNU General Public License version 3, or later
+ */
+
 /** @noinspection PhpIllegalPsrClassPathInspection */
 
 namespace Lcobucci\JWT\Signer;
